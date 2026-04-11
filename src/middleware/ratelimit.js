e85f9969555d2844f43bda6b0b40daefbc5478ea
darshankardile <darshan.kardil@gmail.com>
2026-04-11 14:11:34 +0530

    ratelimit-incre

--- src/middleware/ratelimit.js
+++ src/middleware/ratelimit.js
@@ -7,7 +7,7 @@ dotenv.config()
 
  const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(10, "10 s"),
+  limiter: Ratelimit.slidingWindow(20, "10 s"),
 });
 
 export async function ratelim(req, res, next) {
